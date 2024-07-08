d37e31d3a65b2d865412139ba029cd610cdd1d66
Siddharthkumarrrrr <Siddharthkmarrrrr@gmail.com>
2024-07-08 08:52:36 +0530

    backend

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
-const cors=require("cors");
-const corsOption={
-    origin:"*",
-    optionSucceessStatus:200,
-};
-require('dotenv').config();
-const DB=require('./src/config/db');
-const express=require("express");
-const bodyparser=require('body-parser');
-const adminRouter=require('./src/routes/admin');
-const alumniRouter=require('./src/routes/alumni');
+const cors = require("cors");
+const express = require("express");
+const bodyparser = require('body-parser');
 const cookieParser = require("cookie-parser");
-const messageRouter = require('./src/routes/message');
-const conversationRouter=require('./src/routes/converstion');
+require('dotenv').config();
+const DB = require('./src/config/db'); // Ensure this is correctly configured and connects to your database
 
-const app=express();
+const adminRouter = require('./src/routes/admin');
+const alumniRouter = require('./src/routes/alumni');
+const messageRouter = require('./src/routes/message');
+const conversationRouter = require('./src/routes/converstion');
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+const corsOptions = {
+    origin: 'https://alumcentralbackend-1.onrender.com', // Your frontend domain
+    credentials: true, // Allow cookies to be sent with requests
+    optionsSuccessStatus: 200
+};
+
 app.use(express.json());
-app.use(cors(corsOption));
+app.use(cors(corsOptions));
 app.use('/uploads', express.static('uploads')); // Serve static files from the uploads directory
 app.use(cookieParser());
 
-app.use('/admin',adminRouter);
-app.use('/alumni',alumniRouter);
-app.use('/messages',messageRouter);
-app.use('/users',conversationRouter);
+app.use('/admin', adminRouter);
+app.use('/alumni', alumniRouter);
+app.use('/messages', messageRouter);
+app.use('/users', conversationRouter);
 
-const port=process.env.PORT||8000;
-
-app.listen(port,()=>{
-    console.log(`Server running at port ${port}`);
+app.get("/", (req, res) => {
+    res.send("Server is up and running at port " + port);
 });
 
-app.get("/",(req,res)=>{
-    res.send("Sever is up and running at port"+ port);
-});
-
-app.all("*",(req, res) => {
+app.all("*", (req, res) => {
     res.status(400).json({
         message: "Route does not exist"
-    })
-})
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at port ${port}`);
+});
